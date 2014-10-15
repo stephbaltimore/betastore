0dcb019dd7a781206b6899f09bb8e4b038695681
stephbaltimore <stephbaltimore@gmail.com>
2014-10-15 18:54:16 -0400

    fixed error messages?

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
   test "create a product" do
     product = Product.new
-    assert product.save, product.errors.full_messages
+    refute product.save, product.errors.full_messages
   end
 
   test "name is required" do
@@ -11,4 +11,11 @@ class ProductTest < ActiveSupport::TestCase
     refute product.save, 'name should be required'
     assert product.errors.full_messages.include?("Name can't be blank")
   end
+
+  test "price is present" do
+    product = Product.new
+    refute product.save, 'price should be required'
+    assert product.errors.full_messages.include?("Price is not a number")
+  end
+
 end
