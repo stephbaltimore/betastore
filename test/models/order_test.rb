750fafe7ef48f3ada9cfde3be7a993882b8fa5f5
stephbaltimore <stephbaltimore@gmail.com>
2014-10-15 20:13:30 -0400

    OrderTest start

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "total amount is set when saving order" do
+    order = Order.new(customer: customers(:paul))
+    order.save
+
+    order.line_items.create(
+      product: products(:hat),
+      quantity: 1)
+
+    assert_equal 9.99, order.total_amount
+  end
 end
