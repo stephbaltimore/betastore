aeada49492e14bf84adf053c33fd5269530fa0d1
stephbaltimore <stephbaltimore@gmail.com>
2014-10-16 22:55:09 -0400

    after class update

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
+
   test "total amount is set when saving order" do
+
     order = Order.new(customer: customers(:paul))
     order.save
-
     order.line_items.create(
       product: products(:hat),
-      quantity: 1)
+      quantity: 2)
 
-    assert_equal 9.99, order.total_amount
+    assert_equal.errors.full_messages 19.98, order.total_amount
+
   end
+
 end
