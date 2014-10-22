fb09fd0ac2affa78fbca170f876f5b5edb88efbc
stephbaltimore <stephbaltimore@gmail.com>
2014-10-22 18:47:47 -0400

    random image for products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,7 +4,6 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
-    # @id = params[:id]
     @product = Product.find(params[:id])
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ActiveRecord::Base
 
 
 def image_url
-  'http://1.bp.blogspot.com/-Hn_1JBSNTWo/UETGU9qtCCI/AAAAAAAAPNw/0FnkZvEFDeA/s1600/tumblr_liymnbD1M01qc8ax1o1_500.jpg'
+  image_array = ["http://1.bp.blogspot.com/-Hn_1JBSNTWo/UETGU9qtCCI/AAAAAAAAPNw/0FnkZvEFDeA/s1600/tumblr_liymnbD1M01qc8ax1o1_500.jpg", "http://dayoftheartist.files.wordpress.com/2014/05/lisa-frank.gif", "http://www.theluxuryspot.com/wp-content/uploads/2012/10/Lisa-Frank-princess-kitten.jpg"].sample
 end
 
 end
