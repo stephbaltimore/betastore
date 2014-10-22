c18857d8c33f4abeb4e4a124829aad091a9d7b6c
stephbaltimore <stephbaltimore@gmail.com>
2014-10-22 18:58:48 -0400

    added new product route and template page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Betastore::Application.routes.draw do
   resources :subscriptions
 
   namespace :admin do
+    get '/products/new' => 'products#new', as: 'new_product'
     get '/products/:id' => 'products#show', as: 'product'
     get '/products' => 'products#index'
   end
