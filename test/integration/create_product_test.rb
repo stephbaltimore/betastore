42f8778b70936b610cbad25ba0d4770d5e1c2e17
stephbaltimore <stephbaltimore@gmail.com>
2014-10-27 20:45:21 -0400

    slide Testing Controllers 19 homework

--- test/integration/create_product_test.rb
+++ test/integration/create_product_test.rb
@@ -8,13 +8,14 @@ def test_create_product
   fill_in "Name", with: 'Test Product'
   fill_in "Price", with: '1.00'
   fill_in "Image url", with: 'https://www.google.com/images/srpr/logo11w.png'
-  click_on "Save Changes"
+  click_on "Create Product"
   assert page.has_content?('Product Test Product was created')
   click_on "Test Product"
   click_on "Edit"
-  fill_in "Price", with: '3.00'
+  fill_in "Price", with: '123.45'
   click_on "Update Product"
   assert page.has_content?('Product Test Product was updated')
+  assert page.has_content?('123.45')
   click_on "Test Product"
   click_on "Edit"
   click_on "Delete"
