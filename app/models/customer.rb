77decac1a131ff1a31077284f4663df78b932ecc
stephbaltimore <stephbaltimore@gmail.com>
2014-10-29 19:51:29 -0400

    beginning of bootstrap changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,11 @@ class Customer < ActiveRecord::Base
   has_many :orders
   has_many :line_items, :through => :orders
   has_many :products, :through => :line_items
-  
+
+  validates :name, presence: true, length:{ maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+                     format: { with: VALID_EMAIL_REGEX },
+                     uniqueness: true
 end
