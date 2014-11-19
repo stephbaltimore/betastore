6b1053564b0a172c970fc3b7c0870d2285819abd
stephbaltimore <stephbaltimore@gmail.com>
2014-11-18 19:01:16 -0500

    Couldnt find Cart without an ID stuck point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+    def current_cart
+      @current_cart ||= Cart.find(session[:cart_id])
+    end
+    helper_method :current_cart
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,23 @@ class ProductsController < ApplicationController
     @products = Product.order('name').all
   end
 
+  def add_to_cart
+    # cart = Cart.find_or_create_by(id: session[:cart_id])
+    # session[:cart_id] = cart.id
+
+    if session[:cart_id].blank?
+      cart = Cart.create
+      session[:cart_id] = cart.id
+    else
+      cart = Cart.find(session[:cart_id])
+    end
+
+    product = Product.find(params[:id])
+    cart_item = cart.cart_products.build(product: product)
+    if cart_item.save
+      redirect_to products_path, notice: 'Added item to cart'
+    else
+      redirect_to products_path, notice: 'Oops that didnt work'
+    end
+  end
 end
