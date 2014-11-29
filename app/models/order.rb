a03138f67eeae7b94ebd4c289ac0915a73fdc0ea
stephbaltimore <stephbaltimore@gmail.com>
2014-11-28 23:53:35 -0500

    attempt to make orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,27 @@
 class Order < ActiveRecord::Base
 
   belongs_to :customer
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :products, :through => :line_items
 
-def calculate_order_total_amount
-  self.total_amount = 0
-  for line_item in line_items
-    self.total_amount += line_item.total_amount
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  def calculate_order_total_amount
+    self.total_amount = 0
+    for line_item in line_items
+      self.total_amount += line_item.total_amount
+    end
+    save
   end
-  save
-end
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ActiveRecord::Base
   validates :name, presence: {message: 'is required'}
   validates :price, numericality: { greater_than_or_equal_to: 0, message: 'must be a number' }
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
 end
