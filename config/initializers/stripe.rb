c70b1b8422f87c4c560e8d769bde96cdf114f73e
stephbaltimore <stephbaltimore@gmail.com>
2014-12-08 18:36:46 -0500

    fixed stripe error regarding params

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_LQMcoQgCNw4DnPmhuV1AVhRz'],
-  :secret_key      => ENV['sk_test_WRVkn5rgltWr8gTmlwooc5X1']
+  :publishable_key => ENV.fetch('STRIPE_PUBLISHABLE_KEY','pk_test_LQMcoQgCNw4DnPmhuV1AVhRz'),
+  :secret_key      => ENV.fetch('STRIPE_SECRET_KEY','sk_test_WRVkn5rgltWr8gTmlwooc5X1')
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
