59b984c67c4dcee388a70936fe31bb41a0a45e90
stephbaltimore <stephbaltimore@gmail.com>
2014-12-10 18:33:40 -0500

    commented out worker

--- app/workers/order_worker.rb
+++ app/workers/order_worker.rb
@@ -1,12 +1,11 @@
-class OrderWorker
-    include Sidekiq::Worker
-
-    def perform(order_id)
-      if order = Order.find_by(id: order_id)
-        #TODO: order.process
-        Rails.logger.info "Processing order #{order_id}"
-      else
-        Rails.logger.error "Could not find order with order ID = #{order_id}"
-      end
-    end
-end
+# class OrderWorker
+#     include Sidekiq::Worker
+#
+#     def perform(order_id)
+#       if order = Order.find_by(id: order_id)
+#         Rails.logger.info "Processing order #{order_id}"
+#       else
+#         Rails.logger.error "Could not find order with order ID = #{order_id}"
+#       end
+#     end
+# end
